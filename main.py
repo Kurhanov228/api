8a653b57a446ce29c5869b1c5ec89ed092135683
Kurhanov228 <94191848+Kurhanov228@users.noreply.github.com>
2022-06-10 10:06:39 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+from  pprint import pprint
+headers = {
+    'Authorization': 'Bearer ac8678968bd58d3dddad020cdd2a9c8da75ce925',
+}
+
+#response = requests.get('https://api-ssl.bitly.com/v4/user', headers=headers)
+#token = "ac8678968bd58d3dddad020cdd2a9c8da75ce925"
+
+#pprint(response.json()["emails"][0]["email"])
